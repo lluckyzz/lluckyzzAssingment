7a13a04dbf697ffb009abc698d29d337d3fec27b
lluckyzz <133516714+lluckyzz@users.noreply.github.com>
2023-06-15 21:33:30 +0530

    Add files via upload

---
+++ assingment2.py
@@ -0,0 +1,135 @@
+
+a = 20
+b = 10
+print("<<<---------------------------------               Arithmetic Operator                --------------------------------->>>\n")
+print("If a = ",a , " and b = ",b,"\n")
+print("For addition       (a+b) : ",a+b)
+print("For subtraction    (a-b) : ",a-b)
+print("For Multiplication (a*b) : ",a*b)
+print("For Division       (a/b) : ",a/b)
+print("For modulus        (a%b) : ",a%b)
+print("For floor Division (a//b): ",a//b)
+print("For exponentiation (a**b): ",a**b,"\n")
+
+print("<<<---------------------------------               Assingment Operator                --------------------------------->>>\n")
+print("If a = ",a , " and b = ",b,"\n")
+a=10
+print("Assinging a value                   (a=10) :" ,a)
+a+=10
+print("Assinging a value by increamenting  (a+=10):" ,a)
+a-=10
+print("Assinging a value by decreamenting  (a-=10):" ,a)
+a*=10
+print("Assinging a value by multiplying    (a*=10):" ,a)
+a/=10
+print("Assinging a value by Dividing       (a/=10):" ,a)
+a//=10
+print("Assinging a value by floorDividing (a//=10):" ,a)
+a**=10
+print("Assinging a value by exponential   (a**=10):" ,a,"\n")
+
+print("<<<---------------------------------               Logical  Operator                ------------------------------------>>>\n")
+a=1
+b=0
+print("If a = ",a , " and b = ",b,"\n")
+print("And Operator (a and b) :",a and b)
+print("Or Opreator   (a or b) :", a or b)
+print("Not Opreator (a not b) :",not a,"\n")
+
+print("<<<---------------------------------               Relational Operator               ------------------------------------>>>\n")
+a=30
+b=20
+print("If a = ",a , " and b = ",b,"\n")
+print("Greater than Operator          (a>b)  : " , a>b)
+print("Less than Operator             (a<b)  : " , a<b)
+print("Equal to  Operator             (a==b) : " , a==b)
+print("Greater than Equal to Operator (a>=b) : " , a>=b)
+print("Less than Equal to Operator    (a<=b) : " , a<=b)
+print("Not Equal to Operator          (a!=b) : " , a!=b ,"\n")
+
+print("<<<---------------------------------               Bitwise Operator                  ------------------------------------>>>\n")
+a=2
+b=3
+print("If a = ",a , " and b = ",b,"\n")
+print("Bitwise AND          (a&b): ",a&b)
+print("Bitwise OR           (a|b): ",a|b)
+print("Bitwise XOR          (a^b): ",a^b)
+print("Bitwise Shift left  (a<<b): ",a<<b)
+print("Bitwise Shift Right (a>>b): ",a>>b,"\n")
+print("<<<---------------------------------               Identical Operator                  ------------------------------------>>>\n")
+print("If a = ",a , " and b = ",b,"\n")
+print("is Operator         (a is b):",a is b)
+print("is not Operator (a is not b):", a is not b)
+print("<<<---------------------------------               Membership Operator                  ------------------------------------>>>\n")
+s="Lucky"
+print("if s = ",s)
+print("in Operator (\"L\" in s)    :","L" in s)
+print("in Operator (\"L\" not in s):","L" not in s)
+
+
+# Output========>>>>>>>>>>
+
+"""<<<---------------------------------               Arithmetic Operator                --------------------------------->>>
+
+If a =  20  and b =  10 
+
+For addition       (a+b) :  30
+For subtraction    (a-b) :  10
+For Multiplication (a*b) :  200
+For Division       (a/b) :  2.0
+For modulus        (a%b) :  0
+For floor Division (a//b):  2
+For exponentiation (a**b):  10240000000000 
+
+<<<---------------------------------               Assingment Operator                --------------------------------->>>
+
+If a =  20  and b =  10 
+
+Assinging a value                   (a=10) : 10
+Assinging a value by increamenting  (a+=10): 20
+Assinging a value by decreamenting  (a-=10): 10
+Assinging a value by multiplying    (a*=10): 100
+Assinging a value by Dividing       (a/=10): 10.0
+Assinging a value by floorDividing (a//=10): 1.0
+Assinging a value by exponential   (a**=10): 1.0 
+
+<<<---------------------------------               Logical  Operator                ------------------------------------>>>
+
+If a =  1  and b =  0 
+
+And Operator (a and b) : 0
+Or Opreator   (a or b) : 1
+Not Opreator (a not b) : False 
+
+<<<---------------------------------               Relational Operator               ------------------------------------>>>
+
+If a =  30  and b =  20 
+
+Greater than Operator          (a>b)  :  True
+Less than Operator             (a<b)  :  False
+Equal to  Operator             (a==b) :  False
+Greater than Equal to Operator (a>=b) :  True
+Less than Equal to Operator    (a<=b) :  False
+Not Equal to Operator          (a!=b) :  True 
+
+<<<---------------------------------               Bitwise Operator                  ------------------------------------>>>
+
+If a =  2  and b =  3 
+
+Bitwise AND          (a&b):  2
+Bitwise OR           (a|b):  3
+Bitwise XOR          (a^b):  1
+Bitwise Shift left  (a<<b):  16
+Bitwise Shift Right (a>>b):  0 
+
+<<<---------------------------------               Identical Operator                  ------------------------------------>>>
+
+If a =  2  and b =  3 
+
+is Operator         (a is b): False
+is not Operator (a is not b): True
+<<<---------------------------------               Membership Operator                  ------------------------------------>>>
+
+if s =  Lucky
+in Operator ("L" in s)    : True
+in Operator ("L" not in s): False"""
