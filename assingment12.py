2379397f7bc048085b270f2550611625c70b6c17
lluckyzzz <133516714+lluckyzzz@users.noreply.github.com>
2023-07-26 18:18:20 +0530

    Create assingment12.py

---
+++ assingment12.py
@@ -0,0 +1,18 @@
+# 1) Create a user-defined package to implement respective functions
+
+# 2) The package should have at least 5 modules.
+
+# 3) It should be a menu driven program
+
+# 4) It should be a proper project, such as a management system or a login page
+
+# (if it is a login page then it should navigate to that particular site)
+
+
+# Note: you need to implement file handling and exception handling in this assignment
+
+# (if you want you can use in-built modules such as tkinter or webbrowser or math
+# it's optional)
+
+import empPakage as e
+e.empMain()  # type: ignore
