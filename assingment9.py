632ff3df5e6c4df3ccc126cea88e519abc4a102b
lluckyzzz <133516714+lluckyzzz@users.noreply.github.com>
2023-07-26 18:13:30 +0530

    Add files via upload

---
+++ assingment9.py
@@ -0,0 +1,17 @@
+# 1) Create a module consisting of class holding various data members and member functions.
+# (class can be on various file operations or mathematical operations or string operations)
+
+# 2) Import the above module created and try to implement their member functions.
+
+# 3) Also in the same file, create a user defined exception and implement it.
+
+
+# Note:
+# file operations should include all the file modes
+# mathematical operations should include the functions from math module
+# string operations should include string functions such as split()..etc
+
+# String Operations 
+
+from stringOpreations import *
+Opreations(input("Enter a String : "))
